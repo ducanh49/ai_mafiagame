71743bde3b190c202e5e798a3231b1dd93361f3d
ducanh49 <johnynguyen2006@gmail.com>
2026-01-01 12:24:00 +0100

    cycle dnu, opravane TIE voting a NONE voting

--- script.py
+++ script.py
@@ -8,6 +8,8 @@ load_dotenv()
 with open("game_log.txt", "w", encoding="utf-8") as f:
     f.write("=== AI MAFIA GAME LOG ===\n")
 client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+number_of_days = 1
+game_over = False
 
 # Logovaci funkce - nahrazuje "print()" protoze v txt souboru je to prehlednejsi
 def log(text):
@@ -21,10 +23,11 @@ players = {
     "Baltazar": {"role": "civilian", "personality": "distrustful"},
     "Thomas": {"role": "civilian", "personality": "smart"},
     "Jessica": {"role": "civilian", "personality": "quiet"},
+    "Clara": {"role": "civilian", "personality": "kind, naive"},
+    "Victor" : {"role": "civilian", "personality": "funny, careless"},
 }
 
 alive = list(players.keys())
-
 def ask_ai(name, situation):
     role = players[name]["role"]
     personality = players[name]["personality"]
@@ -40,6 +43,9 @@ def ask_ai(name, situation):
         - Don't admit your true role.
         - Answer maximally with 3 sentences.
         - Speak naturally.
+        - Voting "None" is acceptable if you have no suspicion.
+        - If you vote None too often, civilians may lose.
+
 
         Alive players: {alive_players}
 
@@ -49,29 +55,80 @@ def ask_ai(name, situation):
     response = client.chat.completions.create(model="gpt-4o-mini", messages=[{"role": "user", "content": prompt}])
     return response.choices[0].message.content.strip()
 
-# den - verejna debata
-log("\nDAY - DISCUSSION")
 
-statements = {}
+while not game_over:
+    # noc - killer vybira koho vyradi
+    log(f"\nNIGHT {number_of_days}")
+    # najde z pole "alive" kdo je killer a victims - da do variable
+    killer = [p for p in alive if players[p]["role"] == "killer"][0]
+    victims = [p for p in alive if p != killer]
 
-for name in alive:
-    reply = ask_ai(name, "It is morning. Who do you suspect and why?")
-    statements[name] = reply 
-    log(f"{name}: {reply}\n")
+    reply = ask_ai(killer, f"It is Night {number_of_days}. Choose ONE player to kill from this list: {victims}. Answer with ONE name only")
+    victim = reply.strip()
 
-# hlasovani
-log("\nVOTING")
-votes = {}
+    if victim in victims:
+        alive.remove(victim)
+        log(f"{victim} was killed during the night.")
+    else:
+        log("The killer failed to kill anyone. Noone died tonight.")
+        victim = None
 
-for name in alive:
-    others = [p for p in alive if p != name]
-    reply = ask_ai(name, f"This is what other players have said:\n{statements}\n\nWho do you want to eliminate? Answer with only a name.")
-    vote = reply.split()[0]
-    if vote in others:
-        votes[vote] = votes.get(vote, 0) + 1
-    
-    log(f"{name} votes for: {vote}\n")
+    if killer not in alive:
+        log(f"\n Civilians WIN! The killer was {killer}")
+        break
+    if len(alive) == 2 and killer in alive:
+        log(f"\nKiller WINS! The killer was {killer}")
+        break
 
-lynched = max(votes, key=votes.get)
-log(f"\n{lynched} was voted out.")
-log(f"His/Her role was: {players[lynched]['role']}")
+    # den - verejna debata
+    log("\nDAY - DISCUSSION")
+
+    statements = {}
+
+    if victim:
+        day_info = f"Last night {victim} died"
+    else:
+        day_info = "No one died last night."
+
+    for name in alive:
+        reply = ask_ai(name, f"It is Day {number_of_days}. {day_info}. Who do you suspect and why?")
+        statements[name] = reply 
+        log(f"{name}: {reply}\n")
+
+    # hlasovani
+    log("\nVOTING")
+    votes = {}
+
+    for name in alive:
+        others = [p for p in alive if p != name]
+        reply = ask_ai(name, f"This is what other players have said:\n{statements}\n\nWho do you want to eliminate? Answer with only a name.")
+        vote = reply.split()[0]
+        if vote in others:
+            votes[vote] = votes.get(vote, 0) + 1
+        
+        log(f"{name} votes for: {vote}\n")
+    # NOONE VOTED
+    if not votes: 
+        log("\nNo votes were cast. No one was voted out today.")
+        vote_out = None
+    else: 
+        max_votes = max(votes.values())
+        candidates = [p for p, v in votes.items() if v == max_votes]
+    # TIE
+    if len(candidates) > 1:
+        log("\nVoting ended in a tie. No one was voted out today")
+        vote_out = None
+    else:
+        vote_out = candidates[0]
+        alive.remove(vote_out)
+        log(f"\n{vote_out} was voted out.")
+        log(f"His/Her role was: {players[vote_out]['role']}")
+
+    if vote_out == killer:
+        log(f"\nCivilians WIN! The killer was VOTED OUT")
+        break
+    if len(alive) == 2 and killer in alive:
+        log("\nKILLER WINS! Only one civilian remains.")
+        break
+
+    number_of_days += 1
